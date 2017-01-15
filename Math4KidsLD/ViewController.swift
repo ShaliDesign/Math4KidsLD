56740b6d84af2b1ee62fe7c1f64c76fb8176ce04
Linh&ShawnD <linhshawn-d@LinhShawnDs-MacBook-Pro.local>
2017-01-14 18:56:13 -0800

    Almost done

--- Math4KidsLD/ViewController.swift
+++ Math4KidsLD/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var lbNum1: UILabel!
 
+    @IBOutlet weak var lbNum2: UILabel!
+    
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    setRandom()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setRandom() {
+        let random1 = Int (arc4random_uniform(5)) + 1
+        let random2 = Int (arc4random_uniform(5)) + 1
+        print("\(random1) \(random2)")
+        
+        
+        // Hien thi so
+        
+        lbNum1.text = String(random1)
+        lbNum2.text = String(random2)
+        setResult(randomA: random1, randomB: random2)
+     
+        
+    }
+
+    func setResult(randomA: Int, randomB: Int)
+    
+    {
+        
+    btn1.setTitle(String(randomA), for: .normal)
+    btn2.setTitle(String(randomB), for: .normal)
+    btn3.setTitle(String(sum(p1: randomA), p2: randomB));, for: .normal)
+    
+    }
+
+    func sum(p1: Int, p2: Int) -> Int
+    {
+        return p1+p2;
+    
+    }
+    
     }
 
 
-}
+
 
